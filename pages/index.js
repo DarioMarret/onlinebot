7fa11752ff7b878786fc648b501b4f0ca181bbc4
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-05-30 11:41:38 -0500

    integracion de cuadro de dialogo

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,96 @@
+import React, {useEffect} from 'react'
 import Head from 'next/head'
 
 export default function Home() {
+
+
+  const ventanaSecundaria = (URL) => {
+    window.open(URL, "ventana1", "width=120,height=300,scrollbars=NO")
+  }
+
+  // function statusChangeCallback(response) {
+  //   console.log('statusChangeCallback');
+  //   console.log(response);
+  //   if (response.status === 'connected') {
+  //     testAPI();
+  //   } else {
+  //     document.getElementById('status').innerHTML = 'Please log ' +
+  //       'into this app.';
+  //   }
+  // }
+
+  // function checkLoginState() {
+  //   FB.getLoginStatus(function (response) {
+  //     statusChangeCallback(response);
+  //   });
+  // }
+
+  // window.fbAsyncInit = function () {
+  //   FB.init({
+  //     appId: '{your-app-id}',
+  //     cookie: true,
+  //     xfbml: true,
+  //     version: 'v14.0'
+  //   });
+
+
+  //   FB.getLoginStatus(function (response) {
+  //     statusChangeCallback(response);
+  //   });
+
+  // };
+
+  // Load the SDK asynchronously
+  // (function (d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0];
+  //   if (d.getElementById(id)) return;
+  //   js = d.createElement(s); js.id = id;
+  //   js.src = "https://connect.facebook.net/en_US/sdk.js";
+  //   fjs.parentNode.insertBefore(js, fjs);
+  // }(document, 'script', 'facebook-jssdk'));
+
+  // function testAPI() {
+  //   console.log('Welcome!  Fetching your information.... ');
+  //   FB.api('/me', function (response) {
+  //     console.log('Successful login for: ' + response.name);
+  //     document.getElementById('status').innerHTML =
+  //       'Thanks for logging in, ' + response.name + '!';
+  //   });
+  // }
+
+
+  useEffect(() => {
+    (() => {
+      FB.init({
+        appId: '3176667395950990',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v14.0'
+      });
+    })
+  }, []);
+
   return (
     <div className="container">
       <Head>
         <title>IntelnexoBot</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <meta name="facebook-domain-verification" content="re53pssz1yix7gw93hqijubcmgdwvi" />
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
       </Head>
 
       <main>
         <h1 className="title">
           Prueba de Bot integracion Csml
         </h1>
+        <button onClick={() => ventanaSecundaria("https://www.facebook.com/v14.0/dialog/oauth?app_id=3176667395950990&auth_type=reauthorize&link=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https://developers.facebook.com/tools/explorer")}>
+          Permiso
+        </button>
 
-        <div className="grid">
-
+        <div id="status">
         </div>
       </main>
-
       <footer>
         <a
           href="https://intelnexo.com"
@@ -29,7 +101,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
-
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -175,7 +246,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-      <script src="https://chatbox.csml.dev/script.min.js?token=s41fvi4mkgsaejzcjanuj7gvjbfgj0as" id="clevy-chatbox" async></script> 
     </div>
   )
 }
