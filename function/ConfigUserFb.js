21b350c8485d1d282fbbed83880fb125dd057b1c
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-11 08:04:33 -0500

    tes

--- function/ConfigUserFb.js
+++ function/ConfigUserFb.js
@@ -2,20 +2,21 @@ import axios from "axios";
 
 export const SetuserFb = async (data) => {
 
-    var config = {
-        method: 'post',
-        url: 'https://incoming.xfiv.chat/configuserfb/api/newConfigButton',
-        headers: { 
-          'Authorization': 'Basic dXNlcjE6cGFzczE=', 
-          'Content-Type': 'application/json'
-        },
-        data : data
-      };
+  console.log("datos",data);
+    // var config = {
+    //     method: 'post',
+    //     url: 'https://incoming.xfiv.chat/configuserfb/api/newConfigButton',
+    //     headers: { 
+    //       'Authorization': 'Basic dXNlcjE6cGFzczE=', 
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data : data
+    //   };
 
-    try {
-        const { data } = await axios(config)
-        return data;
-    } catch (error) {
-        return error
-    }
+    // try {
+    //     const { data } = await axios(config)
+    //     return data;
+    // } catch (error) {
+    //     return error
+    // }
 }
