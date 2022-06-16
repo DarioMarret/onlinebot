55c91ef653204c29eb4e120421a8c3527110c086
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-16 12:13:23 -0500

    ngrok_1

--- function/ConfigUserFb.js
+++ function/ConfigUserFb.js
@@ -2,8 +2,23 @@ import axios from "axios";
 
 export const SetuserFb = async (data) => {
   try {
-    const res = await axios.post('https://6d01-45-187-2-162.ngrok.io/api/newConfigButton', data, {headers: {'Authorization': 'Basic dXNlcjE6cGFzczE='}});
-    return res.data;
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Basic dXNlcjE6cGFzczE=");
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify(data);
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("https://6d01-45-187-2-162.ngrok.io/api/newConfigButton", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log("resultado",result))
+      .catch(error => console.log('error', error));
   } catch (error) {
     console.log("error", error);
     return error
