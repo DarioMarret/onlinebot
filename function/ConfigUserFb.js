c2b5741190a240c0659b23d98fe9008301858fc1
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-16 14:30:16 -0500

    ngrok_4

--- function/ConfigUserFb.js
+++ function/ConfigUserFb.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const SetuserFb = async (data) => {
   try {
-    const res = await axios.post('https://6d01-45-187-2-162.ngrok.io/api/newConfigButton', data, {
+    const res = await axios.post('https://6d01-45-187-2-162.ngrok.io/api/newConfigButton', {data}, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Basic dXNlcjE6cGFzczE='
