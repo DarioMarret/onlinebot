f2b5c6e2ae74e0834b9de2a25401ec23d084d3cf
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-16 12:26:05 -0500

    ngrok_1

--- function/ConfigUserFb.js
+++ function/ConfigUserFb.js
@@ -2,25 +2,10 @@ import axios from "axios";
 
 export const SetuserFb = async (data) => {
   try {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic dXNlcjE6cGFzczE=");
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify(data);
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("https://6d01-45-187-2-162.ngrok.io/api/newConfigButton", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log("resultado",result))
-      .catch(error => console.log('error', error));
+    const res = await axios.post('https://hip-glasses-film-45-187-2-162.loca.lt/api/newConfigButton', data, {headers: {'Authorization': 'Basic dXNlcjE6cGFzczE='}});
+    return res.data;
   } catch (error) {
-    console.log("error", error);
+    console.log("error");
     return error
   }
 }
