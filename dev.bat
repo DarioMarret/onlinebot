752eac0115f9c907e269efec019e353ff6e3872f
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-18 10:07:38 -0500

    mejoras

--- dev.bat
+++ dev.bat
@@ -1,5 +1,9 @@
-git checkout master
+@echo off
+set /a whole=(%RANDOM%*65536/32768)+1
+git pull
 git add .
-git status
-git commit -am "made changes"
+set /p cometario="Ingresa un comentario para la commit: "
+git commit -m "%cometario%"
 git push
+echo "Conetarminado: " %cometario%
+echo random dev_%whole%
