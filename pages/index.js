218459e46348e104e9dc33ecc466c347f0efe124
DarioMarret <64284053+DarioMarret@users.noreply.github.com>
2022-06-19 14:02:44 -0500

    reset

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Modal, useModal, Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
 
 import { SetuserFb } from '../function/ConfigUserFb';
-// import FacebookAuth from 'react-facebook-auth';
-import FacebookLogin from 'react-facebook-login';
+import FacebookAuth from 'react-facebook-auth';
 
 export default function Home() {
+
+  const MyFacebookButton = ({ onClick }) => (
+    <div style={{ textAlign: "center", display: "flex", justifyContent: "center", alignItems: "center", padding: "10px", width: "100%" }}>
+      <Button onPress={onClick}>
+        Login with facebook
+      </Button>
+    </div>
+  );
+
   const authenticate = async (response) => {
     console.log("authenticate", response);
     console.log('Good to see you, ' + response.name + '.');
@@ -17,21 +25,11 @@ export default function Home() {
     <div style={{ textAlign: "center", paddingTop: "10%" }}>
       <Text h1 weight="bold">Bot Integracion Xfiv</Text>
       <div style={{ padding: "10px" }} />
-      {/* <FacebookAuth
+      <FacebookAuth
         appId="3176667395950990"
         callback={authenticate}
         component={MyFacebookButton}
-      /> */}
-      <FacebookLogin
-        appId="3176667395950990"
-        autoLoad={false}
-        // fields="name,email,picture"
-        scope="public_profile,pages_messaging,pages_user_gender,pages_show_list"
-        // version='14.0'
-        textButton="Login with Facebook"
-        icon="fa-facebook"
-        callback={authenticate} 
-        />,
+      />
     </div>
 
   )
