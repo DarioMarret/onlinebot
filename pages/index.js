71aef6ad41d9c57772fc605c5ec54163dc37b589
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-02 16:49:51 -0500

    file url

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,8 @@ export default function Home() {
       <div style={{ padding: "10px" }} />
       <FacebookAuth
         appId="3176667395950990"
-        scope={['email', 'user_friends', 'public_profile']}
-        fields="pages_show_lis,pages_manage_metadata,pages_messeging,pages_read_engagement,email,name,picture"
+        scope={['email', 'user_friends', 'public_profile','pages_show_list','pages_manage_metadata','pages_messeging','pages_read_engagement']}
+        fields="email,name,picture"
         callback={authenticate}
         component={MyFacebookButton}
       />
