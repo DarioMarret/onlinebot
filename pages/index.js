9cf11a2d4b81ca2e0b97bca6b656f50a36565ba0
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-23 13:22:07 -0500

    sdk facebook

--- pages/index.js
+++ pages/index.js
@@ -73,8 +73,16 @@ export default function Home() {
         xfbml: true,
         version: 'v14.0'
       })
-      FB.AppEvents.logPageView(); 
+      FB.AppEvents.logPageView();
     }
+
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
   }, []);
 
   return (
