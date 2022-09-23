f6aff3c1eed1b1db819e131ce4d0634c3cdb952c
DarioMarret <javier_dario_marret@hotmail.com>
2022-09-23 12:51:08 -0500

    sdk facebook

--- pages/index.js
+++ pages/index.js
@@ -33,20 +33,59 @@ export default function Home() {
 
     // const { data } = await SetuserFb(response);
     // console.log(data);
-  };
+  }
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+  function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '3176667395950990',
+        cookie: true,
+        xfbml: true,
+        version: '14.0'
+      });
+    }
+  }, []);
 
   return (
     <div style={{ textAlign: "center", paddingTop: "10%" }}>
       <Text h1 weight="bold">Bot Integracion Xfiv</Text>
       <div style={{ padding: "10px" }} />
-      <FacebookAuth
+      {/* <FacebookAuth
         appId="3176667395950990"
         scope={['email', 'user_friends', 'public_profile', 'pages_show_list','pages_manage_metadata', 'pages_read_engagement', 'pages_messaging']}
         fields="email,name,picture"
         callback={authenticate}
         component={MyFacebookButton}
-      />
-    </div>
+      /> */}
 
+      <button
+        scope={['email', 'user_friends', 'public_profile', 'pages_show_list', 'pages_manage_metadata', 'pages_read_engagement', 'pages_messaging']}
+        onClick={checkLoginState}>
+      </button>
+
+      <div id="status">
+      </div>
+    </div>
   )
 }
